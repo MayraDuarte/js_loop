de94725a2e835628417d0d0ac5ea3cc9de15e08f
mayra <126676950+MayraDuarte@users.noreply.github.com>
2023-06-23 16:53:49 -0300

    commit

---
+++ script.js
@@ -0,0 +1,29 @@
+//while
+console.log('while');
+let tabuada = 7;
+let contador = 0;
+
+while ( contador <= 10) {
+    let resultado = tabuada * contador;
+    console.log( tabuada + ' x ' + contador + ' = ' + resultado );
+    contador++;
+}
+
+//For 
+console.log(' For');
+
+for (let contador2 = 0; contador2 <=10; contador2++) {
+    let resultado = tabuada * contador2;
+    console.log(tabuada + ' x ' + contador2 + ' = ' + resultado);
+
+}
+
+//do.while
+console.log('do while');
+
+let contador3 = 0;
+do{
+    let resultado = tabuada * contador3;
+    console.log(tabuada + ' x ' +  contador3 + ' = ' + resultado);
+    contador3++;
+} while ( contador3 <=10);
